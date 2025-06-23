8eaed5e23037f03bb35af06ae55fd6de050f39d3
a1-10 <softito2022@gmail.com>
2025-06-23 15:37:46 +0300

    adonet proje first commit

---
+++ pzt_adoNetCore/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace pzt_adoNetCore.Pages.Users
+{
+	public class IndexModel : PageModel
+    {
+        public List<User> listele = new List<User>();
+
+        public void OnGet()
+        {
+            try
+            {
+                string connectionString = "Server=localhost;Database=Egitim;User Id=SA;Password=reallyStrongPwd123;TrustServerCertificate=True;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "select * from Userlar";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        User islem = new User();
+                        islem.ID = "" + reader.GetInt32(0);
+                        islem.fullName = reader.GetString(1);
+                        islem.email = reader.GetString(2);
+                        islem.telefon =""+ reader.GetInt32(3);
+                        islem.userRole = reader.GetString(4);
+                        islem.password = reader.GetString(4);
+
+                        listele.Add(islem);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            
+            }
+        }
+    }
+}
---
+++ pzt_adoNetCore/Pages/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace pzt_adoNetCore.Pages.Users
+{
+	public class User
+	{
+        public string ID;
+        public string fullName;
+        public string email;
+        public string telefon;
+        public string userRole;
+        public string password;
+
+    }
+}
+
